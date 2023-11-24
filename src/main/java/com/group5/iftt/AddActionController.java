ea4e126ddd8098f61277e1ce58f4ff2093aec0da
amanfredi13 <a.manfredi13@studenti.unisa.it>
2023-11-24 17:54:42 +0100

    Implementazione TimeofDayTrigger.

--- src/main/java/com/group5/iftt/AddActionController.java
+++ src/main/java/com/group5/iftt/AddActionController.java
@@ -3,6 +3,7 @@ package com.group5.iftt;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -21,9 +22,6 @@ public class AddActionController implements Initializable {
     @FXML
     private Button addButton;
 
-    @FXML
-    private Button cancelButton;
-
     private MainController mainController;
     @FXML
     private ComboBox triggerComboBox;
@@ -35,6 +33,8 @@ public class AddActionController implements Initializable {
     private ComboBox comboBoxOra;
     @FXML
     private ComboBox comboBoxMinute;
+    @FXML
+    private Button loadFileButton;
 
     public void setMainController(MainController mainController) {
 
@@ -69,6 +69,7 @@ public class AddActionController implements Initializable {
         comboBoxMinute.setItems(FXCollections.observableArrayList("01","02","03","04","05","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60"));
         comboBoxOra.setVisible(false);
         comboBoxMinute.setVisible(false);
+        loadFileButton.setVisible(false);
         triggerComboBox.valueProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
@@ -85,6 +86,24 @@ public class AddActionController implements Initializable {
             }
         });
 
+        actionComboBox.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                // Verifica se il nuovo valore contiene la stringa "Ora del giorno"
+                if (newValue != null && newValue.contains("Riproduzione audio")) {
+                    // Se sì, rendi visibile il bottone
+                    loadFileButton.setVisible(true);
+                } else {
+                    // Altrimenti, rendi invisibile il bottone
+                    loadFileButton.setVisible(false);
+                }
+            }
+        });
+
     }
 
+    @FXML
+    public void addFileAction(ActionEvent actionEvent) {
+    System.out.printf("stai forte");
+    }
 }
