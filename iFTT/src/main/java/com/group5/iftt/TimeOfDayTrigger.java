f06243ab5380e03d01ea0625b457b02e32143176
amanfredi13 <149245913+amanfredi13@users.noreply.github.com>
2023-11-24 19:03:44 +0100

    Implementazione TimeOfDayTrigge

--- iFTT/src/main/java/com/group5/iftt/TimeOfDayTrigger.java
+++ iFTT/src/main/java/com/group5/iftt/TimeOfDayTrigger.java
@@ -1,11 +1,19 @@
 package com.group5.iftt;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
 public class TimeOfDayTrigger {
-private int hour;
-private int minute;
+private String hour;
+private String minute;
 
 public boolean isValidate(){
- return true;
+    LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+    LocalTime userSelectedTime = userSelectedTime.parse(hour+":"+minute);
+ if(currentTime.equals(userSelectedTime)){
+    return true;
+ }
+ return false;
 }
 public void timeTrigger(int hour, int minute){
     this.hour=hour;
